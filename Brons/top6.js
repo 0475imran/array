0990aaf41cc5fd48375d7a0c5a3a88242bcf1446
0475imran <imran.derraz@student.gildeopleidingen.nl>
2026-06-18 14:07:34 +0200

    Create top6.js

---
+++ Brons/top6.js
@@ -0,0 +1,20 @@
+// Opdracht 1
+const songs = [
+    "Blinding Lights",
+    "Shape of You",
+    "Someone You Loved",
+    "Levitating",
+    "As It Was",
+    "Bad Habits"
+];
+
+// Opdracht 2
+console.log(songs[0]);
+
+// Opdracht 3
+console.log(songs[2]);
+
+// Opdracht 4
+songs.forEach(function(song) {
+    console.log(song);
+});
